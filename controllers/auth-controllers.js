e2adf77194f646f14ecae32c6a693320a603932c
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-06-21 16:59:00 +0300

    Update auth-controllers.js

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -56,7 +56,7 @@ const getCurrent = async (req, res) => {
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(200).json({
+  res.status(204).json({
     message: "No Content",
   });
 };
