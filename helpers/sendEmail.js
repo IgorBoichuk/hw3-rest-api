b8780a9bfe0d22b55e6334911f926c3863b9a0a9
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-07-05 21:27:24 +0300

    summ

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,26 +1,23 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { UKR_NET_EMAIL } = process.env;
+const { UKR_NET_EMAIL, UKR_NET_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.ukr.net",
+  port: 465, // 25, 465, 2525
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  await nodemailer.post("send", { version: "v3.1" }).request({
-    Messages: [
-      {
-        from: {
-          Email: UKR_NET_EMAIL,
-        },
-        to: [
-          {
-            Email: data.to,
-          },
-        ],
-        subject: data.subject,
-        html: data.html,
-      },
-    ],
-  });
-
+  const email = { ...data, from: UKR_NET_EMAIL };
+  await transport.sendMail(email);
   return true;
 };
 
